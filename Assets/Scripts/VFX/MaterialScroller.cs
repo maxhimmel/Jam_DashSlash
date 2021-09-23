e361b6bf809f78ae01ccf05a01a48bbcedd9c24f
Max Himmel <himmelmax@gmail.com>
2021-09-23 16:26:14 -0700

    + Created VFX folder and moved appropriate scripts

--- Assets/Scripts/VFX/MaterialScroller.cs
+++ Assets/Scripts/VFX/MaterialScroller.cs
@@ -7,15 +7,18 @@ namespace DashSlash.Vfx
     [RequireComponent( typeof( Renderer ) )]
     public class MaterialScroller : MonoBehaviour
     {
+		private float DeltaTime => m_useUnscaledTime ? Time.unscaledDeltaTime : Time.deltaTime;
+
 		[SerializeField] private string m_textureName = "_MainTex";
 		[SerializeField] private Vector2 m_scrollSpeed = Vector2.zero;
+		[SerializeField] private bool m_useUnscaledTime = false;
 
         private Renderer m_renderer;
 		private Vector2 m_offset;
 
 		private void Update()
 		{
-			m_offset += m_scrollSpeed * Time.deltaTime;
+			m_offset += m_scrollSpeed * DeltaTime;
 			m_renderer.material.SetTextureOffset( m_textureName, m_offset );
 		}
 
