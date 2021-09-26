2d5748e0350e308594232d5dcefa90fc33d2ee04
Max Himmel <himmelmax@gmail.com>
2021-09-26 16:03:14 -0700

    + Created enemy spawning factories

---
+++ Assets/Scripts/Enemies/Factories/EnemyFactory.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Xam.Gameplay.Patterns;
+
+namespace DashSlash.Gameplay.Enemies.Factories
+{
+    public class EnemyFactory : Factory<Enemy>
+    {
+    }
+}
---
+++ Assets/Scripts/Enemies/Factories/EnemyRangeFactory.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Xam.Gameplay;
+using Xam.Utility.Randomization;
+
+namespace DashSlash.Gameplay.Enemies.Factories
+{
+    [RequireComponent( typeof( EnemyFactory ), typeof( InstancedPlacement ) )]
+    public class EnemyRangeFactory : MonoBehaviour
+    {
+        [SerializeField] private RandomIntRange m_spawnRange = new RandomIntRange( 1, 1 );
+
+        private EnemyFactory m_factory;
+		private IPlacement m_placement;
+
+        public Enemy[] CreateRange()
+		{
+			int spawnCount = m_spawnRange.Evaluate();
+			Enemy[] results = new Enemy[spawnCount];
+
+			for ( int idx = 0; idx < spawnCount; ++idx )
+			{
+				m_placement.GetNextOrientation( idx, spawnCount, out Vector3 spawnPos, out Quaternion spawnRot );
+
+				Debug.Log( spawnPos );
+				Debug.Log( spawnRot );
+				Debug.DrawRay( spawnPos, spawnRot * Vector3.forward, Color.magenta, 1 );
+
+				results[idx] = m_factory.Create( spawnPos, spawnRot );
+			}
+
+			return results;
+		}
+
+		private void Awake()
+		{
+			m_factory = GetComponent<EnemyFactory>();
+			m_placement = GetComponent<IPlacement>();
+		}
+	}
+}
