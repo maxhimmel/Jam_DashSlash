9f88e7101ca703bb053a4c766a76e395c44fa05d
Max Himmel <himmelmax@gmail.com>
2021-09-27 21:18:18 -0700

    + Added cruiser enemy which moves along a sine wave towards direction player is when cruiser is spawned

---
+++ Assets/Scripts/Enemies/Cruiser.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DashSlash.Gameplay.Enemies
+{
+    using Movement;
+
+    public class Cruiser : Enemy
+    {
+		private CharacterMotor m_characterMotor;
+		private WaveMovement m_waveMovement;
+
+		protected override void UpdateState()
+		{
+			m_characterMotor.SetDesiredVelocity( FacingDirection );
+		}
+
+		protected override void OnAwokenFromSpawn()
+		{
+			base.OnAwokenFromSpawn();
+
+			UpdateRotationTowardsPlayer();
+
+			m_waveMovement.enabled = true;
+		}
+
+		protected override void CacheReferences()
+		{
+			base.CacheReferences();
+
+			m_characterMotor = GetComponent<CharacterMotor>();
+			m_waveMovement = GetComponentInChildren<WaveMovement>( true );
+		}
+	}
+}
