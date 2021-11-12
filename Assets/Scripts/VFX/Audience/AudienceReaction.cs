48315255e71714b02f5c2034a1d0d6083628e410
Max <himmelmax@gmail.com>
2021-11-12 07:28:25 -0800

    added reaction radius debug drawing

--- Assets/Scripts/VFX/Audience/AudienceReaction.cs
+++ Assets/Scripts/VFX/Audience/AudienceReaction.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using Cinemachine;
 using Cinemachine.Utility;
 using Xam.Utility.Patterns;
+using Xam.Utility.Extensions;
 
 namespace DashSlash.Vfx.Audiences
 {
@@ -85,5 +86,61 @@ namespace DashSlash.Vfx.Audiences
 				rot = Quaternion.SlerpUnclamped( Quaternion.identity, rot, gain );
 			}
 		}
+
+
+#if UNITY_EDITOR
+		[Header( "Editor / Tools" )]
+		[SerializeField] private Color m_radiusColor = Color.cyan;
+		[SerializeField] private int m_radiusResolution = 20;
+
+		private void OnDrawGizmosSelected()
+		{
+			Vector3 center = transform.position;
+			Vector3 normal = transform.forward;
+
+			UnityEditor.Handles.color = m_radiusColor;
+			UnityEditor.Handles.DrawSolidDisc( center, normal, m_impulseDefinition.m_ImpactRadius );
+
+			for ( int idx = 0; idx < m_radiusResolution; ++idx )
+			{
+				float radiusScale = idx / (float)(m_radiusResolution - 1);
+				float radius = Mathf.Lerp( m_impulseDefinition.m_ImpactRadius, m_impulseDefinition.m_DissipationDistance, radiusScale );
+
+				UnityEditor.Handles.color = m_radiusColor.NewAlpha( DistanceDecay( radius ) );
+				UnityEditor.Handles.DrawWireDisc( center, normal, radius );
+			}
+
+			UnityEditor.Handles.color = m_radiusColor.NewAlpha( 1 );
+			UnityEditor.Handles.DrawWireDisc( center, normal, m_impulseDefinition.m_DissipationDistance );
+		}
+
+		/// <summary>
+		/// Essentially a copy-paste of <see cref="CinemachineImpulseManager.ImpulseEvent.DistanceDecay(float)"/>.
+		/// </summary>
+		/// <param name="distance"></param>
+		/// <returns></returns>
+		public float DistanceDecay( float distance )
+		{
+			float dissipationDistance = m_impulseDefinition.m_DissipationDistance;
+
+			float radius = Mathf.Max( m_impulseDefinition.m_ImpactRadius, 0 );
+			if ( distance < radius )
+				return 1;
+			distance -= radius;
+			if ( distance >= dissipationDistance )
+				return 0;
+
+			switch ( m_impulseDefinition.m_DissipationMode )
+			{
+				default:
+				case CinemachineImpulseManager.ImpulseEvent.DissipationMode.LinearDecay:
+					return Mathf.Lerp( 1, 0, distance / dissipationDistance );
+				case CinemachineImpulseManager.ImpulseEvent.DissipationMode.SoftDecay:
+					return 0.5f * (1 + Mathf.Cos( Mathf.PI * (distance / dissipationDistance) ));
+				case CinemachineImpulseManager.ImpulseEvent.DissipationMode.ExponentialDecay:
+					return 1 - Damper.Damp( 1, dissipationDistance, distance );
+			}
+		}
+#endif
 	}
 }
