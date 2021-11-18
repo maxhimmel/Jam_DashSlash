09ed5163206f1b0d7318ea5bd0dc0971f8f783e9
Max <himmelmax@gmail.com>
2021-11-18 08:08:31 -0800

    added player's googly eyes controller

---
+++ Assets/Scripts/Player/Animations/PlayerGooglyEyesController.cs
@@ -0,0 +1,111 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Xam.Gameplay;
+using Xam.Utility.Extensions;
+
+namespace DashSlash.Gameplay.Player.Animation
+{
+    using Vfx.Googly;
+
+    public class PlayerGooglyEyesController : MonoBehaviour
+    {
+		private bool IsGazing => m_gazeRoutine != null;
+
+		[SerializeField] private float m_gazeDuration = 1;
+
+        private GooglyEyesController m_googlyEyes;
+		private Rigidbody2DBucket m_poiBucket;
+		private Rigidbody2D m_currentPoi;
+		private Coroutine m_gazeRoutine;
+		private float m_changeGazeTargetEndTime;
+
+		private void OnPoiEntered( object sender, Rigidbody2D e )
+		{
+			if ( !IsGazing )
+			{
+				m_currentPoi = GetClosestPoi();
+				m_changeGazeTargetEndTime = Time.timeSinceLevelLoad + m_gazeDuration;
+
+				m_gazeRoutine = StartCoroutine( UpdateGaze() );
+			}
+		}
+
+		private IEnumerator UpdateGaze()
+		{
+			while ( enabled )
+			{
+				if ( m_currentPoi == null || m_changeGazeTargetEndTime <= Time.timeSinceLevelLoad )
+				{
+					m_currentPoi = GetClosestPoi();
+					m_changeGazeTargetEndTime = Time.timeSinceLevelLoad + m_gazeDuration;
+				}
+
+				if ( m_currentPoi == null ) { break; }
+
+				Vector3 selfToPoi = m_currentPoi.position - transform.position.VectorXY();
+				m_googlyEyes.SetDesiredLookDirection( selfToPoi );
+
+				yield return null;
+			}
+
+			m_googlyEyes.SetDesiredLookDirection( Vector3.zero );
+
+			m_gazeRoutine = null;
+		}
+
+		private void OnPoiExited( object sender, Rigidbody2D e )
+		{
+			if ( e == m_currentPoi )
+			{
+				m_currentPoi = GetClosestPoi();
+				m_changeGazeTargetEndTime = Time.timeSinceLevelLoad + m_gazeDuration;
+			}
+		}
+
+		private Rigidbody2D GetClosestPoi()
+		{
+			float closestDistSqr = Mathf.Infinity;
+			Rigidbody2D result = null;
+
+			foreach ( var poi in m_poiBucket.Targets )
+			{
+				if ( !CanLookAtPoi( poi ) ) { continue; }
+
+				Vector3 selfToPoi = poi.position - transform.position.VectorXY();
+				float distSqr = selfToPoi.sqrMagnitude;
+
+				if ( distSqr < closestDistSqr )
+				{
+					closestDistSqr = distSqr;
+					result = poi;
+				}
+			}
+
+			return result;
+		}
+
+		private bool CanLookAtPoi( Rigidbody2D poi )
+		{
+			// TODO: How can we clean out destroyed gameobjects which are still cached by the buckets?
+			if ( poi == null ) { return false; }
+
+			var poiPlayer = poi.GetComponentInParent<PlayerController>();
+			if ( poiPlayer != null ) { return false; }
+
+			return true;
+		}
+
+		private void Start()
+		{
+			m_poiBucket.TargetEntered += OnPoiEntered;
+			m_poiBucket.TargetExited += OnPoiExited;
+		}
+
+		private void Awake()
+		{
+			m_googlyEyes = GetComponentInChildren<GooglyEyesController>();
+			m_poiBucket = GetComponentInChildren<Rigidbody2DBucket>();
+		}
+	}
+}
--- Assets/Scripts/VFX/GooglyEyes/GooglyEye.cs
+++ Assets/Scripts/VFX/GooglyEyes/GooglyEye.cs
@@ -14,15 +14,19 @@ namespace DashSlash.Vfx.Googly
         [SerializeField] private Transform m_pupil = default;
 
         private Vector3 m_lookVelocity;
+        private Vector3 m_desiredLookDirection;
 
         public void SetDesiredLookDirection( Vector3 direction )
 		{
-            direction = m_pupil.InverseTransformDirection( direction );
+            m_desiredLookDirection = m_pupil.InverseTransformDirection( direction );
+		}
 
+		private void LateUpdate()
+        {
             float maxRadius = m_sclera.lossyScale.x / 2f - m_pupil.lossyScale.x / 2f;
-            Vector3 pupilPos = Vector3.ClampMagnitude( direction, maxRadius );
+            Vector3 pupilPos = Vector3.ClampMagnitude( m_desiredLookDirection, maxRadius );
 
             m_pupil.localPosition = Vector3.SmoothDamp( m_pupil.localPosition, pupilPos, ref m_lookVelocity, m_lookDamping );
-		}
-    }
+        }
+	}
 }
