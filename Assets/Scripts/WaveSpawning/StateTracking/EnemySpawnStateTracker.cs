88e24cb5be22aa0f64328b89c6ce0e791d6219d7
Max <himmelmax@gmail.com>
2021-12-11 17:04:32 -0800

    enemy spawn event framework converted to generic spawn events

--- Assets/Scripts/WaveSpawning/StateTracking/EnemySpawnStateTracker.cs
+++ Assets/Scripts/WaveSpawning/StateTracking/EnemySpawnStateTracker.cs
@@ -1,32 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 namespace DashSlash.Gameplay.WaveSpawning
 {
-    using EventQueues;
     using Enemies;
 
-    public class EnemySpawnStateTracker : MonoBehaviour
+    public class EnemySpawnStateTracker : SpawnStateTracker<Enemy>
     {
-        public virtual PlayState State { get; protected set; }
 
-        protected int m_expectedSpawnCount;
-
-        public virtual void PrePlay( int expectedSpawnCount )
-		{
-            State = PlayState.Playing;
-            m_expectedSpawnCount = expectedSpawnCount;
-		}
-
-        public virtual void Spawned( Enemy enemy )
-		{
-
-		}
-
-        public virtual void PostPlay()
-        {
-            State = PlayState.Done;
-        }
     }
 }
