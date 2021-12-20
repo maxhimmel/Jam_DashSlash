a0862234fa0ba9ee12b3bb31feb036efbb6d44cc
Max <himmelmax@gmail.com>
2021-12-20 08:22:14 -0800

    reward spawn events wait for pickups to die/collect before moving on

--- Assets/Scripts/WaveSpawning/RewardSpawnEvent.cs
+++ Assets/Scripts/WaveSpawning/RewardSpawnEvent.cs
@@ -6,9 +6,19 @@ namespace DashSlash.Gameplay.WaveSpawning
 		{
 			base.OnSpawned( spawnable );
 
-			spawnable.Spawn( spawnable.transform.position );
+			var spawnCount = spawnable.Spawn( spawnable.transform.position );
+			NotifyStateTracker( spawnCount );
 
 			Cleanup( spawnable );
+		}
+
+		private void NotifyStateTracker( int lootSpawnCount )
+		{
+			// NOTE: 
+				// We're minusing 1 from the loot spawn count due to the loot spawner being included in the count.
+
+			RewardSpawnStateTracker rewardTracker = m_stateTracker as RewardSpawnStateTracker;
+			rewardTracker.AddExpectedSpawns( lootSpawnCount - 1 );
 		}
 
 		private void Cleanup( LootSpawner spawner )
--- Assets/Scripts/WaveSpawning/StateTracking/RewardSpawnStateTracker.cs
+++ Assets/Scripts/WaveSpawning/StateTracking/RewardSpawnStateTracker.cs
@@ -1,3 +1,4 @@
+using DashSlash.Gameplay.EventQueues;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,5 +7,48 @@ namespace DashSlash.Gameplay.WaveSpawning
 {
     public class RewardSpawnStateTracker : SpawnStateTracker<LootSpawner>
     {
-    }
+		public override PlayState State => IsAnyPickupAlive() ? PlayState.Playing : PlayState.Done;
+
+		private LootSpawner m_lootSpawner;
+		private int m_deadPickupsCount = 0;
+
+		public void AddExpectedSpawns( int count )
+		{
+			m_expectedSpawnCount += count;
+		}
+
+		public override void Spawned( LootSpawner spawnable )
+		{
+			base.Spawned( spawnable );
+
+			m_lootSpawner = spawnable;
+			spawnable.PickupSpawned += OnPickupSpawned;
+		}
+
+		public override void PostPlay()
+		{
+			if ( m_lootSpawner != null )
+			{
+				m_lootSpawner.PickupSpawned -= OnPickupSpawned;
+			}
+		}
+
+		private void OnPickupSpawned( object sender, Pickup pickup )
+		{
+			pickup.Died += OnPickupDied;
+		}
+
+		private void OnPickupDied( object sender, System.EventArgs args )
+		{
+			Pickup pickupSender = sender as Pickup;
+			pickupSender.Died -= OnPickupDied;
+
+			++m_deadPickupsCount;
+		}
+
+		private bool IsAnyPickupAlive()
+		{
+			return m_expectedSpawnCount > m_deadPickupsCount;
+		}
+	}
 }
