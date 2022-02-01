cc17ea3ff093e7ec81c09ae95010da79479628e1
Max <himmelmax@gmail.com>
2022-02-01 08:46:28 -0800

    added dash attack antic

--- Assets/Scripts/Enemies/Dasher.cs
+++ Assets/Scripts/Enemies/Dasher.cs
@@ -1,21 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Xam.Utility.Juicy;
+using Xam.Utility.Randomization;
+using Xam.Utility.Extensions;
 
 namespace DashSlash.Gameplay.Enemies
 {
     using Movement;
 
-    public class Dasher : Enemy
+	public class Dasher : Enemy
     {
 		[Header( "Dasher" )]
 		[SerializeField] private float m_dashOffsetAngle = 15f;
-		[SerializeField] private float m_dashCooldownDuration = 1;
 		[SerializeField] private float m_dashDistance = 5;
+
+		[Space]
+		[SerializeField] private float m_dashAnticDuration = 0.5f;
+		[SerializeField] private float m_dashCooldownDuration = 1;
 
 		private LerpMotor m_motor;
 		private float m_nextDashTime;
 		private int m_angleSign = 1;
+		private ColorBlinker m_dashAnticBlinker;
+		private Coroutine m_dashAnticRoutine;
 
 		protected override void UpdateState()
 		{
@@ -35,8 +43,20 @@ namespace DashSlash.Gameplay.Enemies
 
 		private void UpdateDashState()
 		{
+			UpdateDashAntic();
+
 			m_nextDashTime = Time.timeSinceLevelLoad + m_dashCooldownDuration;
 			m_angleSign *= -1;
+		}
+
+		private void UpdateDashAntic()
+		{
+			m_dashAnticBlinker.Stop();
+
+			float duration = m_dashCooldownDuration - m_dashAnticDuration;
+
+			this.TryStopCoroutine( ref m_dashAnticRoutine );
+			m_dashAnticRoutine = this.StartWaitingForSeconds( duration, m_dashAnticBlinker.Play );
 		}
 
 		private void ApplyMovement()
@@ -68,7 +88,7 @@ namespace DashSlash.Gameplay.Enemies
 		{
 			base.InitReferences();
 
-			m_angleSign = (Random.value >= 0.5f) ? 1 : -1;
+			m_angleSign = RandomUtility.Sign();
 		}
 
 		protected override void CacheReferences()
@@ -76,6 +96,7 @@ namespace DashSlash.Gameplay.Enemies
 			base.CacheReferences();
 
 			m_motor = GetComponent<LerpMotor>();
+			m_dashAnticBlinker = GetComponentInChildren<ColorBlinker>();
 		}
 	}
 }
